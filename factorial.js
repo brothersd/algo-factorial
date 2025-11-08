17a31c8cd776107a7e64f3d4260f1a0f8960339e
Dan Brothers <brothersd@hotmail.com>
2025-11-08 11:43:05 -0700

    Shorthand version

--- factorial.js
+++ factorial.js
@@ -1,12 +1,17 @@
 const num = 0;
 
 function factorial(num) {
-  if (num === 0 || num === 1) {
-    return 1;
-  }else{
-    return num * factorial(num - 1);
-  } 
   
+  //Iteratice way
+  // if (num === 0 || num === 1) {
+  //   return 1;
+  // }else{
+  //   return num * factorial(num - 1);
+  // } 
+
+  //shorthand way
+  return (num === 0 || num === 1) ? 1 : num * factorial(num - 1);
+
 }
 
 module.exports = factorial;
