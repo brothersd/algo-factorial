aa54db2ec6c63388af4b578d457dacc329ba6867
Dan Brothers <brothersd@hotmail.com>
2025-11-08 11:30:56 -0700

    Factorial Practice

--- factorial.js
+++ factorial.js
@@ -1,5 +1,21 @@
+const num = 0;
+
 function factorial(num) {
-  return 0;
+  if (num === 0 || num === 1) {
+    return 1;
+  }else{
+    return num * factorial(num - 1);
+  } 
+  
 }
 
 module.exports = factorial;
+
+
+// console.log(factorial(0));
+// console.log(factorial(1));
+// console.log(factorial(2));
+// console.log(factorial(4));
+// console.log(factorial(8));
+// console.log(factorial(18));
+// console.log(num);
